b82812d060ebb76e1f0c93e223d5aa37146e430a
Fuzail <falsegod81@gmail.com>
2021-03-07 22:39:53 +0530

    optimized method

--- src/Arrays/FindCommon14.java
+++ src/Arrays/FindCommon14.java
@@ -8,41 +8,27 @@ public class FindCommon14
     private int ar1[];
     private int ar2[];
     private int ar3[];
-    private int ar[];
-    private int m;
+    private int commonElementsCount;
 
-    public FindCommon14(int n1, int n2, int n3, int n4)
+    public FindCommon14(int n1, int n2, int n3)
     {
         sc = new Scanner(System.in);
         ar1 = new int[n1];
         ar2 = new int[n2];
         ar3 = new int[n3];
-        ar = new int[n4];
-        m = 0;
+        commonElementsCount = 0;
     }
 
-    public void store()
+    public void store(int[] ar)
     {
-        System.out.println("Enter the values for the first array");
-        for (int i = 0; i < ar1.length; i++)
+        System.out.println("Enter the values for the  array");
+        for (int i = 0; i < ar.length; i++)
         {
-            ar1[i] = sc.nextInt();
-        }
-
-        System.out.println("Enter the values for the second array");
-        for (int i = 0; i < ar2.length; i++)
-        {
-            ar2[i] = sc.nextInt();
-        }
-
-        System.out.println("Enter the values for the third array");
-        for (int i = 0; i < ar3.length; i++)
-        {
-            ar3[i] =sc.nextInt();
+            ar[i] = sc.nextInt();
         }
     }
 
-    public void commonElements()
+    public void commonElements(int[] ar)
     {
         for (int i = 0; i < ar1.length; i++)
         {
@@ -54,23 +40,23 @@ public class FindCommon14
                     {
                         if(ar2[j] == ar3[k])
                         {
-                            m = m + 1;
-                            ar[m-1] = ar3[k];
+                            commonElementsCount++;
+                            ar[commonElementsCount-1] = ar3[k];
                         }
                     }
                 }
             }
         }
-        if(m == 0)
+        if(commonElementsCount == 0)
         {
             System.out.println("There are no common elements present in all 3 arrays");
         }
     }
 
-    public void getCommon()
+    public void getCommon(int[] ar)
     {
         System.out.println("The common elements are ");
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < ar.length; i++)
         {
             System.out.print(ar[i] + " ");
         }
@@ -87,23 +73,16 @@ public class FindCommon14
 
         System.out.println("Enter the size of the third array");
         int n3 = sc.nextInt();
-        int n4 = 0;
-        if (n1 < n2 && n1 < n3)
-        {
-               n4 = n1;
-        }
-        if (n2 < n1 && n2 < n3)
-        {
-            n4 = n2;
-        }
-        if (n3 < n1 && n3 < n2)
-        {
-            n4 = n3;
-        }
-        FindCommon14 s = new FindCommon14(n1,n2,n3,n4);
-        s.store();
-        s.commonElements();
-        s.getCommon();
+        FindCommon14 s = new FindCommon14(n1,n2,n3);
+        s.store(s.ar1);
+        s.store(s.ar2);
+        s.store(s.ar3);
+
+        int min = Math.min(n1, n2);
+        min = Math.min(min, n3);
+        int[] ar = new int[min];
+        s.commonElements(ar);
+        s.getCommon(ar);
     }
 
 }
