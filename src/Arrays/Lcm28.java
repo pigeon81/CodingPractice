89e4b4b51371abd5e446577b9525b863fc7b81a4
Fuzail <falsegod81@gmail.com>
2021-03-11 10:48:53 +0530

    done

--- src/Arrays/Lcm28.java
+++ src/Arrays/Lcm28.java
@@ -8,13 +8,16 @@ public class Lcm28
     private Scanner sc;
 
     public Lcm28()
+
     {
         sc = new Scanner(System.in);
     }
      // use proper names int hcf, int a, int b
-    public void getLcm(int n,int n1,int n2)
+    public void getLcm(int n1,int n2)
     {
         //we know LCM * HCF = A * B;
+        Hcf27 h = new Hcf27();
+        int n = h.getHcf(n1,n2);
         int lcm = (n1 * n2) / n;
         System.out.println("LCM of the two numbers is " +lcm);
     }
@@ -27,12 +30,9 @@ public class Lcm28
         System.out.println("Enter the value of the second number ");
         int n2 = sc.nextInt();
         // do this in lcm method 
-        Hcf27 h = new Hcf27();
-        int n = h.getHcf(n1,n2);
-
 
         Lcm28 s = new Lcm28();
-        s.getLcm(n,n1,n2);
+        s.getLcm(n1,n2);
     }
 
 }
